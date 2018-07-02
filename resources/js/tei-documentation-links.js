4adb50467bb8e79ee09816d6fad33b389a0d332e
hou2zi0 <max.gruentgens@adwmainz.de>
2018-07-02 16:09:46 +0200

    Added classes and polished code snippets.

--- resources/js/tei-documentation-links.js
+++ resources/js/tei-documentation-links.js
@@ -7,11 +7,11 @@ Array.from(codeList)
   .forEach((n) => {
     const text = n.textContent.replace(/</g, "&lt;")
       .replace(/>/g, "&gt;");
-    const regexEl = `&lt;(\/{0,1})([A-Za-z]*?)(\\s.*?.*?){0,1}(\/{0,1})&gt;`;
+    const regexEl = `&lt;(\/{0,1})([-A-Za-z]*?)(\\s.*?.*?){0,1}(\/{0,1})&gt;`;
     const regularExpressionEl = new RegExp(regexEl, 'g');
     const newSnippetEl = text.replace(regularExpressionEl, "<span class='delimiters'>&lt;$1</span><span class='element'><a class='tei-doc-link' href='http://www.tei-c.org/release/doc/tei-p5-doc/en/html/ref-$2.html'>$2</a>$3</span><span class='delimiters'>$4&gt;</span>");
 
-    const regexAttr = `([A-Za-z]*?)="(.*?)"`;
+    const regexAttr = `([A-Za-z:]*?)="(.*?)"`;
     const regularExpressionAttr = new RegExp(regexAttr, 'g');
     const newSnippetAttr = newSnippetEl.replace(regularExpressionAttr, '<span class="attribute">$1</span><span class="delimiters">=</span><span class="value">"$2"</span>')
 
