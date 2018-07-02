960652afc55e60f6309d424743fd7c0920551f08
hou2zi0 <max.gruentgens@adwmainz.de>
2018-07-02 13:17:33 +0200

    Added automatic links to TEI-Documentation in JS folder.

--- resources/js/tei-documentation-links.js
+++ resources/js/tei-documentation-links.js
@@ -1,12 +1,19 @@
-const codeList = document.getElementsByClassName('xml tei');
+const codeList = document.getElementsByClassName('xml tei-doc-link');
 console.log(codeList);
 console.log(codeList[0].textContent);
+//codeList[0].textContent = "Blub";
+let count = 0;
 Array.from(codeList)
   .forEach((n) => {
     const text = n.textContent.replace(/</g, "&lt;")
       .replace(/>/g, "&gt;");
-    const regex = `&lt;([A-Za-z]*?)(\\s.*?.*?\/{0,1}){0,1}&gt;`;
-    const regularExpression = new RegExp(regex, 'g');
-    const newSnippet = text.replace(regularExpression, "&lt;<a class='tei-doc-link' href='http://www.tei-c.org/release/doc/tei-p5-doc/en/html/ref-$1.html'>$1</a>$2&gt;");
-    n.innerHTML = newSnippet;
+    const regexEl = `&lt;(\/{0,1})([A-Za-z]*?)(\\s.*?.*?){0,1}(\/{0,1})&gt;`;
+    const regularExpressionEl = new RegExp(regexEl, 'g');
+    const newSnippetEl = text.replace(regularExpressionEl, "<span class='delimiters'>&lt;$1</span><span class='element'><a class='tei-doc-link' href='http://www.tei-c.org/release/doc/tei-p5-doc/en/html/ref-$2.html'>$2</a>$3</span><span class='delimiters'>$4&gt;</span>");
+
+    const regexAttr = `([A-Za-z]*?)="(.*?)"`;
+    const regularExpressionAttr = new RegExp(regexAttr, 'g');
+    const newSnippetAttr = newSnippetEl.replace(regularExpressionAttr, '<span class="attribute">$1</span><span class="delimiters">=</span>"<span class="value">$2</span>"')
+
+    n.innerHTML = newSnippetAttr;
   });
